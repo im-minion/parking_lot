8fd94d0ccf0d810dd69cf03bdc42db097110c4cc
im-minion <vaibhavminiyar@gmail.com>
2019-09-29 20:58:52 +0530

    updated exception handler class

--- src/main/java/io/vaibhav/auto/parker/exception/ExecutorException.java
+++ src/main/java/io/vaibhav/auto/parker/exception/ExecutorException.java
@@ -1,20 +1,12 @@
-/**
- *
- */
 package io.vaibhav.auto.parker.exception;
 
 public class ExecutorException extends Exception {
     private static final long serialVersionUID = -3552275262672621625L;
 
     private String errorCode = null;    // this will hold system defined error code
-    private Object[] errorParameters = null;    // this will hold parameters for error code/message
 
     public ExecutorException(String message, Throwable throwable) {
         super(message, throwable);
-    }
-
-    public ExecutorException(String message) {
-        super(message);
     }
 
 
@@ -22,21 +14,9 @@ public class ExecutorException extends Exception {
         super(throwable);
     }
 
-    public ExecutorException(String errorCode, String message, Object[] errorParameters) {
-        super(message);
-        this.setErrorCode(errorCode);
-        this.setErrorParameters(errorParameters);
-    }
-
     public ExecutorException(String errorCode, String message, Throwable throwable) {
         super(message, throwable);
         this.setErrorCode(errorCode);
-    }
-
-    public ExecutorException(String errorCode, String message, Object[] errorParameters, Throwable throwable) {
-        super(message, throwable);
-        this.setErrorCode(errorCode);
-        this.setErrorParameters(errorParameters);
     }
 
     public String getErrorCode() {
@@ -47,11 +27,4 @@ public class ExecutorException extends Exception {
         this.errorCode = errorCode;
     }
 
-    public Object[] getErrorParameters() {
-        return errorParameters;
-    }
-
-    public void setErrorParameters(Object[] errorParameters) {
-        this.errorParameters = errorParameters;
-    }
 }
